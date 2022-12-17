14eda946f89801235782a2ea1fb95dddda4676df
powcoder <powcoder@163.com>
2022-12-17 17:10:38 +0800

    CS 6340 Lab 4 typescript

---
+++ FractionMatcher/LevelNodeTest.ts
@@ -0,0 +1,48 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+import { dropShapeToZone } from './LevelNode';
+import { expect } from 'chai';
+import { assert } from 'chai';
+import 'mocha';
+
+describe('test0', () => {
+	it('test ret', () => {
+		let foo = {
+			view: undefined,
+			dropZone: 100,
+			numerator: 1,
+			denominator: 3
+
+		}
+		const result = dropShapeToZone(foo, 1);	 
+		assert.isUndefined(result);
+	});
+});
+
+
+describe('test1', () => {
+	it('test ret', () => {
+		let foo = {
+			view: {
+				indexShape: 1,
+				height: 10,
+				moveToFront: function(){
+
+				}
+			},
+			dropZone: 100,
+			numerator: 1,
+			denominator: 3
+
+		}
+		const result = dropShapeToZone(foo, 1);	 
+		assert.isUndefined(result);
+	});
+});
+
---
+++ README.md
@@ -0,0 +1,9 @@
+# CS 6340 Lab 4 typescript
+# 加微信 powcoder
+
+# QQ 1823890830
+
+# Programming Help Add Wechat powcoder
+
+# Email: powcoder@163.com
+
